08c23b1e9f4e7a572ff86925b8c4e54eb7b3e789
Guillermo Szeliga <gszeliga@gmail.com>
2017-07-02 23:15:59 +0200

    Clean up code

--- src/main/clojure/single_stream.clj
+++ src/main/clojure/single_stream.clj
@@ -1,13 +1,14 @@
 (ns single-stream
-  (require [clojure.core.async :as async]
+  (require [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alt! alts! go thread close! go-loop]]
            [clojure.java.io :as io]))
 
 (defn stream-lines []
-  (let [c (async/chan)]
-    (async/thread
-      (with-open [rdr (io/reader "https://s3.amazonaws.com/carto-1000x/data/yellow_tripdata_2016-01.csv")]
+  (let [c (chan)]
+    (go
+      (with-open [rdr (io/reader "file:///home/gszeliga/development/workspace/carto-db-test/yellow_tripdata_2016-01.csv")]
         (doseq [line (line-seq rdr)]
-          (async/>!! c line))))
+          (>! c line)))
+      (close! c))
     c))
 
 (defn spit-it-out [channel]
@@ -15,22 +16,23 @@
     (async/go (while true (println (async/<! channel))))))
 
 (defn partial-counter [channel]
-  (async/go-loop [total 0]
-    (if-some [_ (async/<! channel)]
+  (go-loop [total 0]
+    (if-some [_ (<! channel)]
       (recur (inc total))
       total)))
 
 (defn count-lines [channel]
-  (for [_ (range 8)]
-    (partial-counter channel)))
+  (let [counters (for [_ (range 8)]
+                   (partial-counter channel))]
+    (async/reduce + 0 (async/merge counters))))
+
+(defn fan-out [in-chan & out-chans]
+  (async/go-loop [msg (<! in-chan)]
+    (when (some? msg)
+      (doseq [c out-chans]
+        (>! c msg))
+      (recur (conj in-chan out-chans)))))
 
 (defn go! []
   (let [c (async/chan)]
-    (async/go
-      (async/>! c "foo")
-      (async/>! c "bar")
-      (async/>! c "baz")
-      (async/close! c))
-    (println (async/<!! (async/reduce + 0 (async/merge (count-lines (stream-lines))))))))
-    ;(let [[v c] (async/alts!! (count-lines c))]
-    ;  (println v))))
+    (println (<!! (count-lines (stream-lines))))))
