34a11a8baa53e821082a1305ca3c3fb617e82774
Guillermo Szeliga <gszeliga@gmail.com>
2017-07-26 00:17:17 +0200

    Patched up aggregation

--- src/main/clojure/single_stream.clj
+++ src/main/clojure/single_stream.clj
@@ -1,5 +1,5 @@
 (ns single-stream
-  (require [clojure.core.async :as async :refer [<! >! <!! >!! timeout chan alt! alts! go thread close! go-loop pipe pipeline]]
+  (require [clojure.core.async :as async :refer [<! >! <!! >!! mult tap timeout chan alt! alts! go thread close! go-loop pipe pipeline]]
            [clojure.java.io :as io]
            [clojure.string :refer [split]]))
 
@@ -23,11 +23,11 @@
                    (partial-counter channel))]
     (async/reduce + 0 (async/merge counters))))
 
-(defn partial-aggregate [fpos channel]
-  (go-loop [c 0 s 0]
+(defn partial-aggregate [channel]
+  (go-loop [c 0 s 0.0]
     (if-some [n (<! channel)]
       (recur (inc c) (+ n s))
-      (/ s c))))
+      [s c])))
 
 (defn field-pos! [fname channel]
   (let [headers (<!! channel)]
@@ -38,11 +38,15 @@
         get-field-fn #(java.lang.Double/parseDouble (nth (split % #",") fpos))
         nchan (pipe channel (chan 1024 (map get-field-fn)))
         aggregators (for [_ (range 8)]
-                      (partial-aggregate fpos nchan))]
+                      (partial-aggregate nchan))]
 
-    (async/reduce + 0 (async/merge aggregators))))
+    (go
+      (let [[s c] (<! (async/reduce #(apply map + [%1 %2]) [0.0 0] (async/merge aggregators)))]
+        (/ s c)))))
 
 (defn go! []
-  (let [c (chan)]
-    (println (<!! (aggregate-field "tip_amount" (stream-lines))))))
-    ;(println (<!! (count-lines (stream-lines))))))
+  (let [lines (mult (stream-lines))
+        aggregate-tap (tap lines (chan 10))
+        count-tap (tap lines (chan 10))]
+    (<!! (async/reduce (fn [_ v] (println v)) "" (async/merge [(aggregate-field "tip_amount" aggregate-tap)
+                                                               (count-lines count-tap)])))))
