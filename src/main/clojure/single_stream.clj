c69e943e7760645fc9b44d5141f53c903898f030
Guillermo Szeliga <gszeliga@gmail.com>
2017-07-31 23:32:37 +0200

    Clean up the code

--- src/main/clojure/single_stream.clj
+++ src/main/clojure/single_stream.clj
@@ -3,50 +3,49 @@
            [clojure.java.io :as io]
            [clojure.string :refer [split]]))
 
-(defn stream-lines []
-  (let [c (chan)]
+(defn stream-lines-from [path]
+  (let [c (chan 1024)]
     (go
-      (with-open [rdr (io/reader "file:///home/gszeliga/development/workspace/carto-db-test/yellow_tripdata_2016-01.csv")]
+      (with-open [rdr (io/reader path)]
         (doseq [line (line-seq rdr)]
           (>! c line)))
       (close! c))
     c))
 
-(defn partial-counter [channel]
+(defn w-counter [channel]
   (go-loop [total 0]
     (if-some [_ (<! channel)]
       (recur (inc total))
       total)))
 
-(defn count-lines [channel]
-  (let [counters (for [_ (range 8)]
-                   (partial-counter channel))]
+(defn count-lines [channel npar]
+  (let [counters (for [_ (range npar)]
+                   (w-counter channel))]
     (async/reduce + 0 (async/merge counters))))
 
-(defn partial-aggregate [channel]
+(defn w-aggregate [channel]
   (go-loop [c 0 s 0.0]
     (if-some [n (<! channel)]
       (recur (inc c) (+ n s))
       [s c])))
 
-(defn field-pos! [fname channel]
-  (let [headers (<!! channel)]
-    (->> (split headers  #",") (map-indexed vector) (filter (fn [[idx v]] (= v fname))) first first)))
+(defn extract-field-fn [fname channel]
+  (let [headers (<!! channel)
+        fidx (->> (split headers  #",") (map-indexed vector) (filter (fn [[_ v]] (= v fname))) first first)]
+    #(java.lang.Double/parseDouble (nth (split % #",") fidx))))
 
-(defn aggregate-field [fname channel]
-  (let [fpos (field-pos! fname channel)
-        get-field-fn #(java.lang.Double/parseDouble (nth (split % #",") fpos))
-        nchan (pipe channel (chan 1024 (map get-field-fn)))
-        aggregators (for [_ (range 8)]
-                      (partial-aggregate nchan))]
-
+(defn aggregate-field [fname channel npar]
+  (let [as-value-fn (extract-field-fn fname channel)
+        fvchan (pipe channel (chan 1024 (map as-value-fn)))
+        aggregators (for [_ (range npar)]
+                      (w-aggregate fvchan))]
     (go
       (let [[s c] (<! (async/reduce #(apply map + [%1 %2]) [0.0 0] (async/merge aggregators)))]
         (/ s c)))))
 
-(defn go! []
-  (let [lines (mult (stream-lines))
-        aggregate-tap (tap lines (chan 10))
-        count-tap (tap lines (chan 10))]
-    (<!! (async/reduce (fn [_ v] (println v)) "" (async/merge [(aggregate-field "tip_amount" aggregate-tap)
-                                                               (count-lines count-tap)])))))
+(defn process! [path fname npar]
+  (let [lines (mult (stream-lines-from path))
+        aggregate-tap (tap lines (chan 1024))
+        count-tap (tap lines (chan 1024))]
+    (<!! (async/reduce (fn [_ v] (println v)) "" (async/merge [(aggregate-field fname aggregate-tap npar)
+                                                               (count-lines count-tap npar)])))))
